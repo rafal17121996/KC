b306489e1e3308378ff4e01ecce4f2a96b0753d3
Rafal <rafal.szczurekk@gmail.com>
2021-01-07 18:43:47 +0100

    HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import StoreProvider from "./store/StoreProvider";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.scss";
 import Navbar from "./components/Navbar/Navbar";
